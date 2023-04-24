3ea2b956bc1bb3eb6deaa223b7db755f88a847ce
MatthijsMu <matthijsmuis2003@gmail.com>
2023-04-24 16:19:51 +0200

    add field, colour, move, piece

---
+++ src/main/java/model/Field.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Field {
+    static final int ROW_SIZE = 8;
+    static final int COL_SIZE = 8;
+
+    private int file;
+    private int rank;
+
+    
+    @Override
+    public String toString() {
+        return String((char)((int) 'a' + file)
+    }
+}
---
+++ src/main/java/model/Piece.java
@@ -0,0 +1,6 @@
+package model;
+
+public enum Piece{
+    W_PAWN, W_KNIGHT, W_BISHOP, W_ROOK, W_QUEEN, W_KING, 
+    B_PAWN, B_KNIGHT, B_BISHOP, B_ROOK, B_QUEEN, B_KING, EMPTY 
+}
