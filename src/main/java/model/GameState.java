164e180373730eaaf5e1b3a974886e1abcfa09f0
MatthijsMu <matthijsmuis2003@gmail.com>
2023-04-24 16:23:44 +0200

    change something in gamestate

--- src/main/java/model/GameState.java
+++ src/main/java/model/GameState.java
@@ -1,5 +1,16 @@
 package model;
 
 public class GameState {
+    
+    static final int BOARD_SIZE = 8;
 
+    private final Piece[][] board = new Piece[BOARD_SIZE][BOARD_SIZE]; 
+
+    
+
+
+
+    public GameState() {
+        board = 
+    }
 }
