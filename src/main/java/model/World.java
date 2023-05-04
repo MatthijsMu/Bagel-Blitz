b527fba51379f1c3c7a9120b8d4cd881b849620b
MatthijsMu <matthijsmuis2003@gmail.com>
2023-05-04 18:47:02 +0200

    change s/getToMove to s/getTomove in World.java

--- src/main/java/model/World.java
+++ src/main/java/model/World.java
@@ -4,6 +4,6 @@ public interface World {
     public Board getBoard();
     public void  setBoard(Board board);
     public Clock getClock();
-    public Colour getToMove();
-    public Colour setToMove();
+    public Colour getTomove();
+    public Colour setTomove();
 }
